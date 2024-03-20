61d24d9f0f30ccd6d77a63e3c10bc0faef49f232
hvacTech0302 <mb030287@proton.me>
2024-03-20 18:12:15 -0400

    started adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import Hero from "./components/hero/hero.component";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.page";
+import NotFound from "./pages/NotFound.page";
 import "./App.css";
+import "./components/navbar/navigation.styles.css";
 
 function App() {
-  return <Hero />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        {/* If invalid URL, route to NotFound */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.page.jsx
@@ -0,0 +1,7 @@
+import Hero from "../components/hero/hero.component";
+
+const Home = () => {
+  return <Hero />;
+};
+
+export default Home;

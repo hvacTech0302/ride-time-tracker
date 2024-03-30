0eab6a31bdb9d326bfcc09c38554e7d5df3f7ac7
hvacTech0302 <mb030287@proton.me>
2024-03-29 20:30:27 -0400

    VIDEO EDITS FOR SAFARI

--- src/components/MainHeader/MainHeader.component.jsx
+++ src/components/MainHeader/MainHeader.component.jsx
@@ -1,13 +1,12 @@
-import MainHeaderVideo from "../../assets/images/Hero/Site Video/main-site-hero-vid.mp4";
 import "./mainHeader.styles.css";
 
 const MainHeader = () => {
   return (
     <section className="container main-header-container">
       <video
-        src={MainHeaderVideo}
-        type={"video/mp4"}
-        preload={"yes"}
+        src="../../assets/images/Hero/Site Video/main-site-hero-vid.mp4"
+        type="video/mp4"
+        preload="yes"
         playsInline
         muted
         autoPlay
